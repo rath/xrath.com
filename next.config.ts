f859418a8fe89ceacf5f2ccfd286a7c451f85558
rath <rath@xrath.com>
2025-06-21 03:24:24 +0200

    build: Add Docker support

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 31536000, // 1 year
   },
+  output: 'standalone',
   async headers() {
     return [
       {
